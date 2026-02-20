52694bc0d0fa245a2486a959f795fd6a07a9d3aa
Yonier-Valencia <valenciayonier834@gmail.com>
2026-02-20 10:18:19 -0500

    Actualización de trabajo-en-clase

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,26 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common'; // Para el *ngFor
+import { FormsModule } from '@angular/forms'; // Para el [(ngModel)]
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'trabajo-en-clase';
+  nuevaTarea: string = '';
+  listaTareas: string[] = [];
+
+  agregarTarea(): void {
+    if (this.nuevaTarea.trim()) {
+      this.listaTareas.push(this.nuevaTarea);
+      this.nuevaTarea = '';
+    }
+  }
+
+  eliminarTarea(index: number): void {
+    this.listaTareas.splice(index, 1);
+  }
 }
